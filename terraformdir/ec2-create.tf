313858a1321ade5b8df4e99d958ecb8db13dcd6b
Samarth Rao <samarth@gmail.com>
2023-07-09 22:29:43 +0000

    .

---
+++ terraformdir/ec2-create.tf
@@ -0,0 +1,125 @@
+resource "aws_security_group" "docker-sg" {
+
+  name = "docker-sg"
+
+  ingress {
+
+    from_port   = 80
+
+    to_port     = 80
+
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+
+    from_port   = 443
+
+    to_port     = 443
+
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+
+    from_port   = 22
+
+    to_port     = 22
+
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+
+    from_port   = 0
+
+    to_port     = 0
+
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
+
+
+
+resource "aws_key_pair" "sambo-key" {
+
+    key_name = "sambo-key"
+
+    public_key = file("sambo-key.pub")
+
+}
+
+
+
+resource "aws_instance" "capstoneawsdocker" {
+
+  ami           = "ami-02f3f602d23f1659d"
+
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.docker-sg.id]
+
+  key_name      = aws_key_pair.sambo-key.key_name
+
+
+
+  tags = {
+
+    Name = "capstone-aws-docker"
+
+  }
+
+
+
+  provisioner "file" {
+
+      source      = "remotedocker.sh"
+
+      destination = "/tmp/remotedocker.sh"
+
+  }
+
+
+
+  provisioner "remote-exec" {
+
+    inline = [
+
+      "chmod +x /tmp/remotedocker.sh",
+
+      "sudo /tmp/remotedocker.sh",
+
+    ]
+
+  }
+
+
+
+  connection {
+
+    host        = self.public_ip
+
+    type        = "ssh"
+
+    user        = "ec2-user"
+
+    private_key = file("sambo-key")
+
+  }
+
+}
+
+
---
+++ terraformdir/providers.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+
+    access_key = "ASIAXTTGYA7GHW6EBWMV"
+
+    secret_key = "ctMhOW84aJUJMXNsMRrYZfQCOS7bFjNf/4rxmDU2"
+
+    token = "FwoGZXIvYXdzECkaDHhNzYjvXkvwecEUPSKyAejQT28YYpSont9agDGiv2RTcF6Hd8nQrlZKyZIQnJ85ibCKq0sYbU68s4H9/0L0kdqXJuLF9qm9h5ElNe956SdxpBUDFOdjx4m7IyEVjuR4LfJGl5iYjnyzIwQ3jDuhje6/HJRcZnNzwNXe2z6245/4ei8DAwmpejKDBNeP8+WX2fuxrj00hy69zA0cBdcMcVpPvLu2Anx1qwY5IlK7rEWFLfmD30X3C5d6c5DmJio1pewooaqrpQYyLXVq6nnAONBQFSFlSHyBpAnEvFkcvprztY3MRQ/K/XMRk10kGDxHFbdG85h7Qg=="
+
+    region = "us-east-1"
+
+}
